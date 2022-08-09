950c13b1f588bd43dd7cb01cf5b6d7bfbfb2cede
Mel Bourgeois <mason.bourgeois@gmail.com>
2022-08-09 01:41:45 -0500

    [doom] switch to doom-material

--- doom/config.el
+++ doom/config.el
@@ -37,8 +37,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function.
-(setq doom-theme 'doom-city-lights)
-;; Other themes I like: doom-rouge, doom-challenger-deep, doom-monokai-spectrum, doom-monokai-octagon, doom-city-lights
+(setq doom-theme 'doom-material)
+;; Other themes I like: doom-rouge, doom-challenger-deep, doom-monokai-spectrum, doom-monokai-octagon, doom-city-lights, doom-material
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
