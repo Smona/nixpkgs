839c4b5edac5f9f3f645c1818028a496e9380f4d
Mel Bourgeois <mason.bourgeois@gmail.com>
2022-08-23 18:07:14 -0500

    sync enabled gnome extensions and fill out some more
    ...which i only noticed were missing because of the synced enabling.
    more reproducability ftw!

--- desktops/gnome.nix
+++ desktops/gnome.nix
@@ -1,26 +1,39 @@
 { config, lib, pkgs, ... }:
 
-{
-  imports = [ ./gui.nix ];
-
-  home.packages = with pkgs.gnomeExtensions; [
+let
+  extensions = with pkgs.gnomeExtensions; [
     # Gnome goodness
     always-indicator
     audio-switcher-40
     autohide-battery
     burn-my-windows
     clipboard-indicator
+    coverflow-alt-tab
     dash-to-dock
+    desktop-icons-ng-ding
     espresso # Requires gnome 40 (Ubuntu >= jammy jellyfish)
     gesture-improvements
     hide-keyboard-layout
     middle-click-to-close-in-overview
     noannoyance-2 # TODO: settings dialog broken
+    openweather
+    pip-on-top # Fix firefox PIP pinning on wayland
     remove-alttab-delay-v2
     system-monitor # Requires Ubuntu > 20.04
-    transparent-window # TODO: this doesn't seem to work
     unite
     user-themes
     windownavigator
   ];
+in {
+  imports = [ ./gui.nix ];
+
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/shell".enabled-extensions =
+        builtins.map (e: e.extensionUuid) extensions;
+    };
+  };
+
+  home.packages = extensions;
 }
