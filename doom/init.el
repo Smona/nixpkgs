3c4b57bab6709b15973cb130773f923c53485cbb
Mel Bourgeois <mason.bourgeois@gmail.com>
2022-09-21 23:35:09 -0500

    [doom] enable docker lsp

--- doom/init.el
+++ doom/init.el
@@ -91,7 +91,7 @@
        ;;biblio            ; Writes a PhD for you (citation needed)
        (debugger +lsp)          ; FIXME stepping through code, to help you add bugs
        direnv
-       docker
+       (docker +lsp)
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
