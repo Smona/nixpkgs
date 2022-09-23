f4d9d5c12f0ced5d76af6c1d576db7e909198cf5
Mel Bourgeois <mason.bourgeois@gmail.com>
2022-09-23 15:11:51 -0500

    [doom] switch to tokyo-night theme

--- doom/config.el
+++ doom/config.el
@@ -40,8 +40,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function.
-(setq doom-theme 'doom-material)
-;; Other themes I like: doom-rouge, doom-challenger-deep, doom-monokai-spectrum, doom-monokai-octagon, doom-city-lights, doom-material
+(setq doom-theme 'doom-tokyo-night)
+;; Other themes I like: doom-rouge, doom-challenger-deep, doom-monokai-spectrum, doom-monokai-octagon, doom-city-lights, doom-material, doom-palenight, doom-tokyo-night
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
