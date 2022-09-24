8b22e660907c7c84b5f63da58d711f2d801d0a35
Mel Bourgeois <mason.bourgeois@gmail.com>
2022-09-24 01:52:27 -0500

    [doom] Hide tilde fringe

--- doom/init.el
+++ doom/init.el
@@ -49,7 +49,7 @@
                          ; This fixes NerdFont icons in vterm as long as a valid
                          ; doom-unicode-font is set.
        (vc-gutter +pretty)         ; vcs diff in the fringe
-       vi-tilde-fringe   ; fringe tildes to mark beyond EOB
+       ;; vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
        zen               ; distraction-free coding or writing
