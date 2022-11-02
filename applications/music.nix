00c5bc3a0a7bc889fb9d53fc42a84b72608ddb6c
Mel Bourgeois <mason.bourgeois@gmail.com>
2022-11-01 21:50:55 -0500

    Fix optionals call

--- applications/music.nix
+++ applications/music.nix
@@ -3,7 +3,7 @@
 {
   config = lib.mkIf config.roles.music {
     home.packages = with pkgs;
-      [ real_time_config_quick_scan ] ++ (lib.lists.optional config.graphical [
+      [ real_time_config_quick_scan ] ++ (lib.lists.optionals config.graphical [
         bitwig-studio
         reaper
         qpwgraph
