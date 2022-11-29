a6192dbcb825782a332549f4d4e55d0b98cbe4e0
Mel Bourgeois <mason.bourgeois@gmail.com>
2022-11-28 19:51:44 -0600

    Remove reaper

--- applications/music.nix
+++ applications/music.nix
@@ -3,10 +3,7 @@
 {
   config = lib.mkIf config.roles.music {
     home.packages = with pkgs;
-      [ real_time_config_quick_scan ] ++ (lib.lists.optionals config.graphical [
-        bitwig-studio
-        reaper
-        qpwgraph
-      ]);
+      [ real_time_config_quick_scan ]
+      ++ (lib.lists.optionals config.graphical [ bitwig-studio qpwgraph ]);
   };
 }
