41ba793b1717bc97582471b11949e6a74e2d65b4
Mel Bourgeois <mason.bourgeois@gmail.com>
2022-12-17 03:48:24 -0600

    [doom] more org-mode beautification

--- doom/config.el
+++ doom/config.el
@@ -96,26 +96,14 @@
 
 ;; The below was borrowed from https://zzamboni.org/post/beautifying-org-mode-in-emacs/
 ;; TODO: inherit font color from default face
- (let* ((variable-tuple
-          (cond ((x-list-fonts "ETBembo")         '(:font "ETBembo"))
-                ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
-                ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
-                ((x-list-fonts "Verdana")         '(:font "Verdana"))
-                ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
-                (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-         (headline           `(:inherit default :weight bold)))
+ (defun my/org-mode-hook ()
+  (set-face-attribute 'org-level-4 nil :height 1.1)
+  (set-face-attribute 'org-level-3 nil :height 1.2)
+  (set-face-attribute 'org-level-2 nil :height 1.3)
+  (set-face-attribute 'org-level-1 nil :height 1.4)
+  (set-face-attribute 'org-document-title nil :height 2.0 :underline nil))
+(add-hook 'org-load-hook #'my/org-mode-hook)
 
-    (custom-theme-set-faces
-     'user
-     `(org-level-8 ((t (,@headline ,@variable-tuple))))
-     `(org-level-7 ((t (,@headline ,@variable-tuple))))
-     `(org-level-6 ((t (,@headline ,@variable-tuple))))
-     `(org-level-5 ((t (,@headline ,@variable-tuple))))
-     `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-     `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.2))))
-     `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.3))))
-     `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.4))))
-     `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
   (custom-theme-set-faces
    'user
    '(org-block ((t (:inherit fixed-pitch))))
@@ -123,7 +111,7 @@
    '(org-document-info ((t (:foreground "dark orange"))))
    '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
    '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
-   '(org-link ((t (:foreground "royal blue" :underline t))))
+   '(org-link ((t (:underline t))))
    '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
    '(org-property-value ((t (:inherit fixed-pitch))) t)
    '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
@@ -192,7 +180,10 @@
 
 (after! org
         ;; Use non-monospace font by default in org mode
-        ;; (add-hook 'org-mode-hook 'variable-pitch-mode)
+        (add-hook 'org-mode-hook 'variable-pitch-mode)
+        ;; TODO: make this a bit bigger
+        (setq org-ellipsis "⤵")
+        (setq org-hidden-keywords '(title))
         ;;
         ;; TODO: add author & date in preamble: https://emacs.stackexchange.com/questions/45191/how-to-place-author-and-date-under-title
         (setq org-html-postamble "
