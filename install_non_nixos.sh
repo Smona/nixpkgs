4cf02f12b55f2ed4da98a5b7214f64424a7715c0
Mel Bourgeois <mason.bourgeois@gmail.com>
2023-06-27 00:14:55 -0500

    install script fixes

--- install_non_nixos.sh
+++ install_non_nixos.sh
@@ -6,8 +6,11 @@ set -e
 # NOTE: if installing on a system multiple people will be using, manually
 #       install the multi-user installation instead.
 # See: https://nixos.org/manual/nix/stable/installation/installing-binary.html#single-user-installation
-sh <(curl -L https://nixos.org/nix/install) --no-daemon
-. $HOME/.nix-profile/etc/profile.d/nix.sh
+
+if [[ ! -e /nix/store ]]; then
+    sh <(curl -L https://nixos.org/nix/install) --daemon
+    . $HOME/.nix-profile/etc/profile.d/nix.sh
+fi
 
 ./install.sh
 
