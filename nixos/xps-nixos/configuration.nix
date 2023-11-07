cf1896d1a2421bc07a4bab0122f24dae09c35531
Mel Bourgeois <mason.bourgeois@gmail.com>
2023-11-06 20:39:40 -0600

    [nixos] set perf policy

--- nixos/xps-nixos/configuration.nix
+++ nixos/xps-nixos/configuration.nix
@@ -173,6 +173,8 @@
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
     };
   };
 
