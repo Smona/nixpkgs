af4fce214ab68608c21e3696dba996f6b48bb227
Mel Bourgeois <mason.bourgeois@gmail.com>
2024-09-17 20:49:20 -0500

    [wlroots] more intense evening dimming

--- desktops/wlroots/common.nix
+++ desktops/wlroots/common.nix
@@ -16,7 +16,7 @@ commonOptions
   execStart = [
     "squeekboard"
     "tablet_mode_switch"
-    "gammastep-indicator -t 6500K:3800K"
+    "gammastep-indicator -t 6500K:3200K"
     "swaync"
     # Mostly just needed for 1password system authentication, so I can use the SSH agent
     # I used to use the gnome agent, but the deepin one just looks nicer, and appears to
