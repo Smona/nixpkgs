b605f79280aa49180ff57096b7192ba1424eb70f
Mel Bourgeois <mason.bourgeois@gmail.com>
2026-05-02 04:13:48 -0600

    inline luma-nixos configuration.nix

--- modules/hosts/luma-nixos.nix
+++ modules/hosts/luma-nixos.nix
@@ -1,15 +1,96 @@
 # WSL installation
-{ inputs, ... }:
+#
+# This was my first NixOS machine, and I rarely use Windows these days,
+# so it's gotten a bit out of date with my current best practices.
+{ inputs, self, ... }:
 {
   flake.nixosConfigurations."luma-nixos" = inputs.nixpkgs.lib.nixosSystem {
-    specialArgs = {
-      inherit inputs;
-    };
-
     modules = [
       inputs.nixos-wsl.nixosModules.wsl
       inputs.home-manager.nixosModules.home-manager
-      ../../nixos/luma-nixos/configuration.nix
+      self.nixosModules.luma-nixos
     ];
   };
+
+  flake.nixosModules.luma-nixos =
+    {
+      config,
+      lib,
+      pkgs,
+      inputs,
+      ...
+    }:
+    {
+      wsl.enable = true;
+      wsl.defaultUser = "smona";
+
+      nix = {
+        # This will add each flake input as a registry
+        # To make nix3 commands consistent with your flake
+        registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+        # This will additionally add your inputs to the system's legacy channels
+        # Making legacy nix commands consistent as well, awesome!
+        nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+        extraOptions = ''
+          experimental-features = nix-command flakes
+        '';
+      };
+      nixpkgs = {
+        hostPlatform = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+
+      networking.hostName = "luma-nixos";
+
+      environment.systemPackages = with pkgs; [
+        git
+        vim
+      ];
+
+      programs.zsh.enable = true;
+      users.defaultUserShell = pkgs.zsh;
+
+      programs._1password-gui = {
+        enable = true;
+        polkitPolicyOwners = [ "smona" ];
+      };
+
+      users.users.smona = {
+        isNormalUser = true;
+        description = "Mel Bourgeois";
+        extraGroups = [ "wheel" ];
+        uid = 1000;
+      };
+      home-manager.users.smona =
+        { pkgs, config, ... }:
+        {
+          imports = [ ../../home.nix ];
+
+          home.username = "smona";
+          home.homeDirectory = "/home/smona";
+          home.stateVersion = "22.05";
+
+          dconf.enable = true;
+
+          _1passwordBinary = "/mnt/c/Users/mason/AppData/Local/Microsoft/WindowsApps/wslg.exe -d NixOS -- ${pkgs._1password-gui}/bin/1password";
+
+          # Let Home Manager install and manage itself.
+          programs.home-manager.enable = true;
+        };
+
+      home-manager.useGlobalPkgs = true;
+      home-manager.extraSpecialArgs = {
+        inherit inputs;
+        system = "x86_64-linux";
+      };
+
+      # This value determines the NixOS release from which the default
+      # settings for stateful data, like file locations and database versions
+      # on your system were taken. It's perfectly fine and recommended to leave
+      # this value at the release version of the first install of this system.
+      # Before changing this value read the documentation for this option
+      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+      system.stateVersion = "23.05"; # Did you read the comment?
+    };
 }
