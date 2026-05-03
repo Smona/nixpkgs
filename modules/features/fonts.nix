055c13b3186ed00f0058cbe01020a615f735edf3
Mel Bourgeois <mason.bourgeois@gmail.com>
2026-05-02 20:13:03 -0600

    move monolisa inline

--- modules/features/fonts.nix
+++ modules/features/fonts.nix
@@ -1,24 +1,32 @@
 # Fonts I like, in order of preference: MonoLisa, Cascadia Code, FiraCode, Dank Mono, JetBrains Mono
 # Fonts to try: FantasqueSansMono, Inconsolata, Victor Mono
-{ ... }:
+{ self, ... }:
 {
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages.monolisa = pkgs.callPackage (builtins.fetchGit {
+        url = "https://gitlab.com/Smona/monolisa.git";
+        ref = "main";
+        rev = "537b8613fc850cf0c90bb240c3412bb30f1e7b44";
+      }) { };
+    };
+
   flake.homeModules.fonts =
     { pkgs, ... }:
-    let
-      monolisa = pkgs.callPackage (import ../../pkgs/monolisa.nix) { };
-    in
     {
-      home.packages = with pkgs; [
-        # required by Emacs for monospaced icons
-        nerd-fonts.symbols-only
-        noto-fonts-color-emoji # Required by Emacs
-        source-serif # Required by Emacs
-        source-sans
-        rounded-mgenplus # Japanese font support
-        monolisa
-        # M$ fonts
-        corefonts
-        vista-fonts
-      ];
+      home.packages =
+        (with pkgs; [
+          # required by Emacs for monospaced icons
+          nerd-fonts.symbols-only
+          noto-fonts-color-emoji # Required by Emacs
+          source-serif # Required by Emacs
+          source-sans
+          rounded-mgenplus # Japanese font support
+          # M$ fonts
+          corefonts
+          vista-fonts
+        ])
+        ++ [ self.packages.${pkgs.stdenv.hostPlatform.system}.monolisa ];
     };
 }
